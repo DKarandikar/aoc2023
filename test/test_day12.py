998dfe8496ecbbcfccff36c9eaf75410a6129bf6
DKarandikar <daniel.karandikar@zoho.com>
2023-12-12 12:18:57 +0000

    feat do day 12 part 1
    brute force has come back to bite me for part 2

---
+++ test/test_day12.py
@@ -0,0 +1,32 @@
+from src.day12 import HotSprings
+
+test_input = """???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1"""
+
+
+def test_parse():
+    springs = HotSprings.from_str(test_input.split("\n")[0])
+
+    assert springs.row == "???.###"
+    assert springs.groups == [1, 1, 3]
+
+
+def test_determine_char():
+    springs = HotSprings.from_str(test_input.split("\n")[0])
+
+    assert springs.determine_char(springs.row) == "#??.###"
+
+    assert springs.determine_best() == "#.#.###"
+
+
+def test_arrangements():
+    assert HotSprings.from_str(test_input.split("\n")[0]).arrangements() == 1
+    assert HotSprings.from_str(test_input.split("\n")[1]).arrangements() == 4
+    assert HotSprings.from_str(test_input.split("\n")[2]).arrangements() == 1
+    assert HotSprings.from_str(test_input.split("\n")[3]).arrangements() == 1
+    assert HotSprings.from_str(test_input.split("\n")[4]).arrangements() == 4
+    assert HotSprings.from_str(test_input.split("\n")[5]).arrangements() == 10
