942d38cae3125635bc1f72c1d97ab86498f0abe2
DKarandikar <daniel.karandikar@zoho.com>
2023-12-18 13:12:01 +0000

    feat: do day 16 part 1

---
+++ src/day16.py
@@ -0,0 +1,134 @@
+from dataclasses import dataclass
+from typing import Literal
+
+Direction = Literal['r', 'l', 'u', 'd']
+
+
+@dataclass
+class Beam:
+    location: tuple[int, int]
+    dir: Direction
+
+    def __str__(self):
+        return f"{self.dir},{self.location[0]},{self.location[1]}"
+
+    def __hash__(self):
+        return hash(self.__str__())
+
+    def step(self) -> 'Beam':
+        x, y = self.location
+        match self.dir:
+            case 'r':
+                return Beam((x + 1, y), self.dir)
+            case 'l':
+                return Beam((x - 1, y), self.dir)
+            case 'u':
+                return Beam((x, y - 1), self.dir)
+            case 'd':
+                return Beam((x, y + 1), self.dir)
+
+
+@dataclass
+class State:
+    next: list[Beam]
+    existing: dict[tuple[int, int], set[Direction]]
+
+    def key(self) -> str:
+        all_beams = [*self.next]
+
+        for k, v in self.existing.items():
+            for dir in v:
+                all_beams.append(Beam(k, dir))
+
+        return "".join(sorted([str(x) for x in all_beams]))
+
+
+@dataclass
+class Grid:
+    rows: list[str]
+
+    @property
+    def height(self):
+        return len(self.rows)
+
+    @property
+    def width(self):
+        return len(self.rows[0])
+
+    @staticmethod
+    def from_str(s: str) -> 'Grid':
+        return Grid([x for x in s.split("\n")])
+
+    def get(self, x: int, y: int) -> str:
+        return self.rows[y][x]
+
+    def calculate_energized(self) -> int:
+        state = State([Beam((0, 0), 'r')], {})
+        so_far = {state.key()}
+        while True:
+            state = self.do_step(state)
+            key = state.key()
+            if key in so_far:
+                break
+            else:
+                so_far.add(key)
+
+        return len(state.existing)
+
+    def beam_inside(self, b: Beam) -> bool:
+        return (0 <= b.location[0] < self.width) and (0 <= b.location[1] < self.height)
+
+    def do_step(self, s: State) -> State:
+        new_state = State([], s.existing)
+        for beam in s.next:
+            dirs = new_state.existing.setdefault(beam.location, set())
+            dirs.add(beam.dir)
+
+            action = self.get(beam.location[0], beam.location[1])
+
+            match action:
+                case '.':
+                    new_state.next.append(beam.step())
+
+                case '|':
+                    if beam.dir == 'r' or beam.dir == 'l':
+                        new_state.next.append(Beam(beam.location, 'd').step())
+                        new_state.next.append(Beam(beam.location, 'u').step())
+                    else:
+                        new_state.next.append(beam.step())
+
+                case '-':
+                    if beam.dir == 'u' or beam.dir == 'd':
+                        new_state.next.append(Beam(beam.location, 'r').step())
+                        new_state.next.append(Beam(beam.location, 'l').step())
+                    else:
+                        new_state.next.append(beam.step())
+
+                case '/':
+                    match beam.dir:
+                        case 'd': new_state.next.append(Beam(beam.location, 'l').step())
+                        case 'u': new_state.next.append(Beam(beam.location, 'r').step())
+                        case 'l': new_state.next.append(Beam(beam.location, 'd').step())
+                        case 'r': new_state.next.append(Beam(beam.location, 'u').step())
+
+                case '\\':
+                    match beam.dir:
+                        case 'd': new_state.next.append(Beam(beam.location, 'r').step())
+                        case 'u': new_state.next.append(Beam(beam.location, 'l').step())
+                        case 'l': new_state.next.append(Beam(beam.location, 'u').step())
+                        case 'r': new_state.next.append(Beam(beam.location, 'd').step())
+
+        new_state.next = list(set(filter(lambda b: self.beam_inside(b), new_state.next)))
+
+        return new_state
+
+
+def main():
+    with open("../data/day16.txt") as f:
+        lines = f.read()
+
+    print(f"Day 16 part 1 is: {Grid.from_str(lines).calculate_energized()}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ test/test_day16.py
@@ -0,0 +1,38 @@
+from src.day16 import Grid, State, Beam
+
+test_input = """.|...\\....
+|.-.\\.....
+.....|-...
+........|.
+..........
+.........\\
+..../.\\\\..
+.-.-/..|..
+.|....-|.\\
+..//.|...."""
+
+
+def test_energized():
+    grid = Grid.from_str(test_input)
+
+    assert grid.calculate_energized() == 46
+
+
+def test_do_step_dot():
+    grid = Grid.from_str(test_input)
+    state = State([Beam((0, 0), 'r')], {})
+
+    next_state = grid.do_step(state)
+
+    assert next_state.next == [Beam((1, 0), 'r')]
+    assert next_state.existing == {(0, 0): {'r'}}
+
+
+def test_do_step_split():
+    grid = Grid.from_str(test_input)
+    state = State([Beam((1, 0), 'r')], {(0, 0): {'r'}})
+
+    next_state = grid.do_step(state)
+
+    assert next_state.next == [Beam((1, 1), 'd')]
+    assert next_state.existing == {(0, 0): {'r'}, (1, 0): {'r'}}
